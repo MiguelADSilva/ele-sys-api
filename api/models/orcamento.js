74a89c5b4204dc6668bbf8d835a30eee216f4b61
MiguelADSilva <miguelaozinho9910@gmail.com>
2021-11-07 22:29:19 +0000

    testing

--- api/models/orcamento.js
+++ api/models/orcamento.js
@@ -3,9 +3,7 @@ const orcamentoInfo = require("../models/materials");
 
 const orcamentoSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  orcamentoInfo: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "orcamentoInfo" },
-  ],
+  orcamentoInfo: [{ type: mongoose.Schema.Types.ObjectId }],
 });
 
 module.exports = mongoose.model("Orcamento", orcamentoSchema);
