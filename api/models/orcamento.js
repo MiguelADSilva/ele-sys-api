2066f35ebd0e7a7f36a6a2b2e2fe9c81b4ef38cb
MiguelADSilva <miguelaozinho9910@gmail.com>
2021-11-15 00:09:57 +0000

    refactor code orcamentos

--- api/models/orcamento.js
+++ api/models/orcamento.js
@@ -2,8 +2,24 @@ const mongoose = require("mongoose");
 const orcamentoInfo = require("../models/materials");
 
 const orcamentoSchema = mongoose.Schema({
-  orcamento: [
-    { type: mongoose.Schema.Types.ObjectId, ref: orcamentoInfo },
+  id: mongoose.Schema.Types.ObjectId,
+  cableName: [
+    { type: String, ref: orcamentoInfo },
+  ],
+  type: [
+    { type: String, ref: orcamentoInfo },
+  ],
+  cableType: [
+    { type: String, ref: orcamentoInfo }
+  ],
+  meters: [
+    { type: String, ref: orcamentoInfo }
+  ],
+  price: [
+    { type: String, ref: orcamentoInfo }
+  ],
+  imageURL: [
+    { type: String, ref: orcamentoInfo }
   ],
 });
 
