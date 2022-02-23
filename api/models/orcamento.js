3d7977b4a84aa9b26365e91657323a4b38fc9fc8
MiguelSilva <43246456+MiguelADSilva@users.noreply.github.com>
2022-02-23 04:27:19 +0000

    testing

--- api/models/orcamento.js
+++ api/models/orcamento.js
@@ -1,19 +1,21 @@
 const mongoose = require("mongoose");
 
-const orcamentoSchema = mongoose.Schema({
-  orcamento_id: mongoose.Schema.Types.ObjectId,
-  orcamentoName: { type: String, require: true, unique: true },
-  lists: [
-    {
-      _id: { type: String, require: true, unique: true },
-      cableName: { type: String, require: true, unique: true },
-      type: { type: String, require: true },
-      cableType: { type: String, require: true },
-      meters: { type: String, require: true },
-      price: { type: String, require: true },
-      imageURL: { type: String, require: true },
-    },
-  ],
-});
+const orcamentoSchema = mongoose.Schema([
+  {
+    orcamento_id: mongoose.Schema.Types.ObjectId,
+    orcamentoName: { type: String, require: true, unique: true },
+    lists: [
+      {
+        _id: { type: String, require: true, unique: true },
+        cableName: { type: String, require: true, unique: true },
+        type: { type: String, require: true },
+        cableType: { type: String, require: true },
+        meters: { type: String, require: true },
+        price: { type: String, require: true },
+        imageURL: { type: String, require: true },
+      },
+    ],
+  },
+]);
 
 module.exports = mongoose.model("Orcamento", orcamentoSchema);
