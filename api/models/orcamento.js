5151390769f47dcd7a64858e9d0ff8b44859e806
MiguelSilva <43246456+MiguelADSilva@users.noreply.github.com>
2022-02-23 20:14:10 +0000

    refactor

--- api/models/orcamento.js
+++ api/models/orcamento.js
@@ -5,13 +5,13 @@ const orcamentoSchema = mongoose.Schema({
   orcamentoName: { type: String, require: true, unique: true },
   lists: [
     {
-      idmaterial: { type: String, require: true },
-      cableNameMaterial: { type: String, require: true, unique: true },
-      typeMaterial: { type: String, require: true },
-      cableTypeMaterial: { type: String, require: true },
-      metersMaterial: { type: String, require: true },
-      priceMaterial: { type: String, require: true },
-      imageURLMaterial: { type: String, require: true },
+      _id: { type: String, require: true },
+      cableName: { type: String, require: true, unique: true },
+      type: { type: String, require: true },
+      cableType: { type: String, require: true },
+      meters: { type: String, require: true },
+      price: { type: String, require: true },
+      imageURL: { type: String, require: true },
     },
   ],
 });
