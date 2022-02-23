e0d60af3dedc7491d9629a695c4a282d520157ce
MiguelSilva <43246456+MiguelADSilva@users.noreply.github.com>
2022-02-23 22:06:20 +0000

    refactor

--- api/models/orcamento.js
+++ api/models/orcamento.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 
 const orcamentoSchema = mongoose.Schema({
-  orcamentoId: mongoose.Schema.Types.ObjectId,
+  _id: mongoose.Schema.Types.ObjectId,
   orcamentoName: { type: String, require: true, unique: true },
-  material: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Materials",
-    require: true,
-  },
+  material: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Materials",
+      require: true,
+    },
+  ],
 });
 
 module.exports = mongoose.model("Orcamento", orcamentoSchema);
