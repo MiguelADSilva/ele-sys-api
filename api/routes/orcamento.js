83e7c011d230d0dd676b267522a576beea2d2d58
MiguelADSilva <miguelaozinho9910@gmail.com>
2022-06-18 00:57:24 +0100

    refactor

--- api/routes/orcamento.js
+++ api/routes/orcamento.js
@@ -5,6 +5,6 @@ const OrcamentoControllers = require("../controllers/orcamento");
 
 router.post("/saveOrcamento", OrcamentoControllers.saveOrcamento);
 router.get("/getOrcamento", OrcamentoControllers.get_Orcamentos);
-router.delete("/deleteOrcamento/:nameOrcamento", OrcamentoControllers.deleteOrcamentos);
+router.delete("/deleteOrcamento/:_id", OrcamentoControllers.deleteOrcamentos);
 
 module.exports = router;
