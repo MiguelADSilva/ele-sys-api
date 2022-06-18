6f13ebd0a210dcc36e9a34497a838e5200753dec
MiguelADSilva <miguelaozinho9910@gmail.com>
2022-06-18 01:30:23 +0100

    refactor

--- api/controllers/materials.js
+++ api/controllers/materials.js
@@ -60,7 +60,7 @@ exports.get_Materials = (req, res, next) => {
 
 exports.deleteMaterials = (req, res, next) => {
   Materials
-    .findByIdAndDelete( req.params._id )
+    .findByIdAndDelete( req.params.id )
     .exec()
     .then(res => {
       res.status(200).json({
