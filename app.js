65f0c9bbffe69c04adfa457491b9e725c08b4e4e
MiguelADSilva <miguelaozinho9910@gmail.com>
2022-12-14 23:31:09 +0000

    refactor

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+  res.setHeader('Access-Control-Allow-Credentials', true);
   if (req.method === "OPTIONS") {
     res.setHeader("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
     return res.status(200).json({});
